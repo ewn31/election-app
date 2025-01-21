b930470a5b4429229af36eb93ede6444c0bc0573
Epoupa Nyadjroh <epoupaw@gmail.com>
2025-01-21 08:29:14 +0100

    Changes made during the week of 14-17 of January 2025

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const appRouter = require('./routes/appRouter');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
+//const session = require('express-session');
+const {getAuthInfo} = require('./lib/dbConnect.js');
+const validateUser = require('./lib/secureRoute.js');
 const cfg = {
     port:3000,
-    url:"localhost"
+    url:"localhost",
+    unproctected_routes:['/', '/register', '/admin']
 }
 const port = process.env.PORT
 
@@ -18,25 +21,42 @@ app.set('views', './views')
 app.use(express.urlencoded({ extended:true }));
 app.use(compression());
 app.use(cookieParser());
-app.use(session({
+/*app.use(session({
     secret: 'qwerty',
     resave:false,
     saveUninitialized:false
-}))
-app.use((req, res, next)=>{
-    res.setHeader("ngrok-skip-browser-warning", "true");
-    const c = req.cookies;
-    const u = req.url;
-    console.log(c);
-    if(c.session || (u === "/") || (u === "/admin") || (u === "/register")) next();
-    else {
-        //res.render('login',{title:"login",message:""});
-        res.redirect("/");
-    }
-})
-app.use('/', appRouter);
+}))*/
 
 app.use(express.static('static'));
+
+//app.use(validateUser);
+
+/*app.use((req, res, next)=>{
+    res.setHeader("ngrok-skip-browser-warning", "true");
+    const c = req.cookies;
+    const route = req.url;
+    (async () => {
+        try {
+            const authinfo = await getAuthInfo(req.cookies.user);
+            console.log(c, route, authinfo);
+            if((route === "/") || (route === "/admin") || (route === "/register")) next();
+            else if((authinfo.USER === c.user) && (authinfo.TOKEN === c.session)) next();
+            else {
+                res.send("Forbidden");
+            }
+            //if(!(authTokens.includes(c.session)) && (u !== "/") || (u !== "/admin") || (u !== "/register")) next();
+            //if(!(authTokens.includes(c.session))) res.redirect("/");
+            /*else {
+                //res.render('login',{title:"login",message:""});
+                res.redirect("/");
+            }*/
+        /*} catch (error) {
+            console.log(error);
+        }
+    })();
+})*/
+
+app.use('/', appRouter);
 
 app.use((req, res)=>{
     res.status(404).send("Page not found")
--- routes/appRouter.js
+++ routes/appRouter.js
@@ -1,54 +1,89 @@
 const express = require("express");
-const {addStudent, updateStudent, verifyUser, verifyAdmin, addElection, getElections, getElection, addCandidate, getStudent, getStudentElections, getCandidates, voteCandidates, getElectionStatus, registerVote, getHistory, updateElection, deleteElection} = require('../lib/dbConnect');
+const {addStudent, updateStudent, verifyUser, verifyAdmin, addElection, getElections, getElection, addCandidate, getStudent, getStudentElections, getCandidates, voteCandidates, getElectionStatus, registerVote, getHistory, updateElection, deleteElection, addAuthinfo, getAuthInfo, deleteAuthinfo} = require('../lib/dbConnect');
 const formidable = require("formidable");
+const {verifyToken, generateToken} = require('../lib/token');
 //const session = require("express-session");
 
 const appRouter = express.Router();
-
-appRouter.all('/', (req, res)=>{
-    if (req.method === 'GET'){
-        res.render("login", {title:"login", message:"" , feedback:""});
-    }
-    if (req.method === 'POST'){
-        (async ()=>{
-            try {
-                const accessGranted = await verifyUser(req.body)
-                if (accessGranted) {
-                    res.cookie("session", "123qwe", {maxAge: (1000*60*15)});
-                    res.cookie("matricule", req.body.matricule, {maxAge:(1000*60*15)});
-                    res.redirect(`/student/${req.body.matricule}`);}
-                else res.render("login",{title:"login", message:"incorrect username or password", feedback:""})
-            } catch (error) {
-                console.log(error)
+//function to verify the id if users for protected routes
+async function verifyUserId(res, id, token) {
+        try {
+            const authinfo = await getAuthInfo(id);
+            console.log("in verify user", id, token, authinfo);
+            if(authinfo === null) res.send('forbidden');
+            else if((authinfo.USER === id) && (authinfo.TOKEN === token)) next()
+            else{
+                res.send('forbidden')
             }
-        })();
-    }
+        } catch (error) {
+            console.log(error)
+        }
+}
+
+// login route
+appRouter.get('/', (req, res)=>{
+    res.render("login", {title:"login", message:"" , feedback:""});
+})
+appRouter.post('/', (req, res)=>{
+    (async ()=>{
+        //const authinfo = await getAuthInfo(req.cookies.user);
+        try {
+            const accessGranted = await verifyUser(req.body);
+            console.log(accessGranted);
+            if (accessGranted) {
+                const user = req.body.matricule;
+                const token = generateToken(user);
+                (async () => {
+                    try {
+                        await addAuthinfo({token:token, user:user});
+                    } catch (error) {
+                        console.log(error);
+                    }
+                })();
+                res.cookie("session", token);
+                res.cookie("user", req.body.matricule);
+                res.redirect(`/student/${req.body.matricule}`);}
+            else res.render("login",{title:"login", message:"incorrect username or password", feedback:""})
+        } catch (error) {
+            console.log(error)
+        }
+    })();
 })
 
-appRouter.all('/admin', (req, res)=>{
-    if (req.method === 'GET'){
-        res.render("admin", {title:"admin", message:""});
-    }
-    if (req.method === 'POST'){
-        (async ()=>{
-            try {
-                const accessGranted = await verifyAdmin(req.body);
-                if (accessGranted){
-                    res.cookie("session", "123qwe", {maxAge: (1000*60*15)}); 
-                    res.redirect("/elections");
-                }
-                else res.redirect('/admin');
-            } catch (error) {
-                console.log(error)
-            }
-        })();
-    }
+//admin route
+appRouter.get('/admin', (req, res)=>{
+    res.render("admin", {title:"admin", message:""});
 });
-    
-appRouter.all("/student/:id", (req, res)=>{
-    if (req.method === 'GET'){
+appRouter.post('/admin', (req, res)=>{
+    (async ()=>{
+        try {
+            const accessGranted = await verifyAdmin(req.body);
+            const user = req.body.username;
+            const token =  generateToken(user);
+            (async () => {
+                try {
+                    await addAuthinfo({token:token, user:user});
+                } catch (error) {
+                    console.log(error);
+                }
+            })();
+            if (accessGranted){
+                res.cookie("session", token);
+                res.cookie("user", user); 
+                res.redirect("/elections");
+            }
+            else res.redirect('/admin');
+        } catch (error) {
+            console.log(error)
+        }
+    })();
+})
+
+ //student route   
+appRouter.get("/student/:id", (req, res)=>{
         (async () => {
             try {
+                await verifyUserId(res, req.params.id, req.cookies.session)
                 const student = await getStudent(req.params.id);
                 console.log(student);
                 const elections = await getStudentElections(student[0]);
@@ -58,58 +93,57 @@ appRouter.all("/student/:id", (req, res)=>{
                 console.log(error);
             }
         })();
-    } 
-    
-    if (req.method === 'POST'){
-        res.send("voting page");
-    }
 })
-appRouter.all("/vote/:id", (req, res)=>{
+
+//vote route
+appRouter.get("/vote/:id", (req, res)=>{
     const info = (req.params.id).split("-");
     const id = info[0];
     const matricule = info[1];
     console.log(req.params.id, info, id, matricule);
-    if (req.method === 'GET'){
         (async()=>{
             try {
+                await verifyUserId(res, matricule, req.cookies.session)
                 const candidates = await getCandidates(id);
                 console.log(candidates);
                 res.render('vote', {title:"vote",candidates:candidates, id:id, matricule})
             } catch (error) {
                 console.log(error);
             }
-        })();
-    }
-    if(req.method === 'POST'){
-        (async () => {
-            const election_id = id;
-            const data = req.body;
-            try {
-                const status = await voteCandidates(election_id, data);
-                await registerVote(id, matricule);
-                console.log(status);
-                const student = await getStudent(req.cookies.matricule);
-                console.log(student);
-                const elections = await getStudentElections(student[0]);
-                console.log(elections);
-                res.render("student", {title:"vote",data:student[0], elections:elections, feedback:status})
-            } catch (error) {
-                console.log(error)
-                res.send("failed")
-            }
-        })();
-    }
-        
-            
+        })();       
 })
 
+appRouter.post("/vote/:id", (req, res)=>{
+    const info = (req.params.id).split("-");
+    const id = info[0];
+    const matricule = info[1];
+    (async () => {
+        const election_id = id;
+        const data = req.body;
+        try {
+            await verifyUserId(res, matricule, req.cookies.session);
+            const status = await voteCandidates(election_id, data);
+            await registerVote(id, matricule);
+            console.log(status);
+            const student = await getStudent(req.cookies.matricule);
+            console.log(student);
+            const elections = await getStudentElections(student[0]);
+            console.log(elections);
+            res.render("student", {title:"vote",data:student[0], elections:elections, feedback:status})
+        } catch (error) {
+            console.log(error)
+            res.send("failed")
+        }
+    })();
+})
 
-appRouter.all('/register', (req, res)=>{
-    if (req.method === 'GET'){
-        res.render('registration', {title: "registration"})
-    }
-    if (req.method === 'POST'){
-       (async ()=>{
+//register route
+appRouter.get('/register', (req, res)=>{
+    res.render('registration', {title: "registration"})
+})
+
+appRouter.post('/register', (req, res)=>{
+    (async ()=>{
         try {
             await addStudent(req.body)
             res.render('login', {title:"Login", message:"", feedback:"Registration Complete"})
@@ -118,37 +152,44 @@ appRouter.all('/register', (req, res)=>{
             res.render('login', {title:"Login", message:"", feedback:"Registration failed"})
         }
        })();
-    }
 })
+
+//election route
 async function renderElections (res, feedback) {
     const elections = await getElections();
     console.log(elections);
     const f = feedback || "Welcome Back";
     res.render("elections",{title:"elections", elections:elections, feedback:f})
 };
-appRouter.all("/elections", (req, res)=>{
-    if(req.method === 'GET'){
+appRouter.get("/elections", (req, res)=>{
+    (async () => {
+        await verifyUserId(res, 'ADMIN', req.cookies.session);
         renderElections(res, "Welcome back");
-    }
-    if(req.method === 'POST'){
-        (async () => {
-            try {
-                await addElection(req.body)
-                const elections = await getElections();
-                console.log(elections);
-                res.render("elections",{title:"elections", elections:elections, feedback:"Election Added Successfully"})
-            } catch (error) {
-                console.log(error)
-                const elections = await getElections();
-                console.log(elections);
-                res.render("elections",{title:"elections", elections:elections, feedback:"Failed to Add Election"})
-            }
-        })()
-        
-    }
+    })()
 })
-appRouter.all('/elections/:id', (req, res)=>{
-    if(req.method === 'GET'){
+
+appRouter.post('/elections', (req, res)=>{
+    (async () => {
+        try {
+            //have to modify so that the user reads the admin
+            await verifyUserId('ADMIN', req.cookies.session)
+            await addElection(req.body)
+            const elections = await getElections();
+            console.log(elections);
+            res.render("elections",{title:"elections", elections:elections, feedback:"Election Added Successfully"})
+        } catch (error) {
+            console.log(error)
+            const elections = await getElections();
+            console.log(elections);
+            res.render("elections",{title:"elections", elections:elections, feedback:"Failed to Add Election"})
+        }
+    })()
+})
+
+appRouter.get('/elections/:id', (req, res)=>{
+        (async () => {
+            await verifyUserId(res, req.cookies.user, req.cookies.session);
+        })()
         console.log(req.params.id);
         const p = (req.params.id).split("-");
         const id = p[0];
@@ -165,37 +206,40 @@ appRouter.all('/elections/:id', (req, res)=>{
         res.cookie("link", req.params.id);
         res.cookie("n", n);
         res.render('addCandidates',{title:"Add Candidate", link:req.params.id, id:id, name:n, feedback:''});
-    }
-    if(req.method === 'POST'){
-        (async () => {
-            console.log(req.body);
-            try {
-                await addCandidate(req.body);
-                const id = req.cookies.elec_id;
-                const n = req.cookies.n;
-                const link = req.cookies.link;
-                console.log(req.cookies);
-                res.clearCookie("elec_id");
-                res.clearCookie("n");
-                res.clearCookie("link");
-                res.render('addCandidates',{title:"Add Candidate", link:link, id:id, name:n, feedback:true});
-            } catch (error) {
-                console.log(error);
-                const id = req.cookies.elec_id;
-                const n = req.cookies.n;
-                const link = req.cookies.link;
-                res.clearCookie("elec_id");
-                res.clearCookie("n");
-                res.clearCookie("link");
-                res.render('addCandidates',{title:"Add Candidate",link:link, id:id, name:n, feedback:false});
-            }
-        })();
-    }
 })
+
+appRouter.post('/elections/:id', (req, res)=>{
+    (async () => {
+        console.log(req.body);
+        try {
+            await verifyUserId(res, req.cookies.user, req.cookies.session)
+            await addCandidate(req.body);
+            const id = req.cookies.elec_id;
+            const n = req.cookies.n;
+            const link = req.cookies.link;
+            console.log(req.cookies);
+            res.clearCookie("elec_id");
+            res.clearCookie("n");
+            res.clearCookie("link");
+            res.render('addCandidates',{title:"Add Candidate", link:link, id:id, name:n, feedback:true});
+        } catch (error) {
+            console.log(error);
+            const id = req.cookies.elec_id;
+            const n = req.cookies.n;
+            const link = req.cookies.link;
+            res.clearCookie("elec_id");
+            res.clearCookie("n");
+            res.clearCookie("link");
+            res.render('addCandidates',{title:"Add Candidate",link:link, id:id, name:n, feedback:false});
+        }
+    })();
+})
+
 appRouter.get("/election-status/:id",(req, res)=>{
     (async () => {
         id = req.params.id;
         try {
+            await verifyUserId(res, req.cookies.user, req.cookies.session)
             const electionStatus = await getElectionStatus(id)
             console.log(electionStatus);
             res.render('electionStatus', {title:'election status', data:electionStatus})
@@ -207,6 +251,7 @@ appRouter.get("/election-status/:id",(req, res)=>{
 appRouter.get("/history",(req, res)=>{
     (async () => {
         try {
+            await verifyUserId(res, req.cookies.user, req.cookies.session)
             const package = await getHistory();
             package.title = "History"
             res.render("history", package)
@@ -219,6 +264,7 @@ appRouter.get("/candidates/:id", (req, res) =>{
     (async () => {
         const election_id = req.params.id;
         try {
+            await verifyUserId(res, req.cookies.user, req.cookies.session)
             const candidates = await getCandidates(election_id);
             console.log("candidates:", candidates);
             res.json(candidates);
@@ -229,15 +275,27 @@ appRouter.get("/candidates/:id", (req, res) =>{
     })();
 })
 appRouter.get("/logout", (req, res)=>{
+    (async () => {
+        await deleteAuthinfo(req.cookies.user);
+    })()
     if(req.cookies.session){
         res.clearCookie("session");
     }
-    if(req.cookies.matricule){
-       res.clearCookie("matricule");
+    if(req.cookies.user){
+       res.clearCookie("user");
+    }
+    if(req.cookies.elec_id){
+        res.clearCookie("elec_id")
+    }
+    if(req.cookies.link){
+        res.clearCookie("link")
+    }
+    if(req.cookies.n){
+        res.clearCookie('n')
     }
     res.redirect("/");
 })
-appRouter.all("/uploads", (req, res)=>{
+/*appRouter.all("/uploads", (req, res)=>{
     if(req.method === 'GET'){
         res.render('upload', {title:'Upload file'})
     }
@@ -253,12 +311,12 @@ appRouter.all("/uploads", (req, res)=>{
             }
         }))
     }
-})
-appRouter.all('/update-student', (req, res)=>{
-    if(req.method === "GET"){
+})*/
+appRouter.get('/update-student', (req, res)=>{
         (async () => {
             try {
-                const student = await getStudent(req.cookies.matricule);
+                await verifyUserId(req.cookies.user, req.cookies.session);
+                const student = await getStudent(req.cookies.user);
                 console.log(student);
                 const elections = await getStudentElections(student[0]);
                 console.log(elections);
@@ -267,53 +325,63 @@ appRouter.all('/update-student', (req, res)=>{
                 console.log(error);
             }
         })();
-    }
-    if(req.method === 'POST'){
-        (async () => {
-            try {
-                await updateStudent(req.body)
-                res.send('profile modified')
-            } catch (error) {
-                console.log(error)
-            }
-        })()
-    }
 })
-appRouter.all('/modify-election/:id',(req, res)=>{
-    if(req.method === 'GET'){
+
+appRouter.put('/update-student', (req, res)=>{
+    //console.log(req.body)
+    (async () => {
+        try {
+            //await verifyUserId(req.cookies.user, req.cookies.session)
+            //if(req.cookies.user === req.body.matricule){
+            console.log('In update student',req.body)
+            await updateStudent(req.body)
+            res.send('profile modified')
+            //}else res.send('forbidden');
+        } catch (error) {
+            console.log(error)
+        }
+    })()
+})
+appRouter.get('/modify-election/:id',(req, res)=>{
         (async () => {
             const election = await getElection(req.params.id);
             console.log(election);
             res.render('modifyElection',{title:"Modify Election",election});
         })();
-    }
-    if(req.method === 'POST'){
-        (async () => {
-            const data = req.body;
-            const id = req.params.id;
-            try {
-                const feedback = await updateElection(data,id);
-                res.send(feedback);
-            } catch (error) {
-                console.log(error)
-            }
-        })();
-    }
 })
-appRouter.get('/delete-election/:id', (req, res)=>{
+appRouter.put('/modidfy-election/:id', (req, res)=>{
+    (async () => {
+        const data = req.body;
+        const id = req.params.id;
+        try {
+            const feedback = await updateElection(data,id);
+            res.send(feedback);
+        } catch (error) {
+            console.log(error)
+            res.send(feedback);
+        }
+    })();
+})
+appRouter.delete('/delete-election/:id', (req, res)=>{
     const id = req.params.id;
     (async () => {
         try {
             const feedback = await deleteElection(id);
-            if (feedback === true){
+            /*if (feedback === true){
                 await renderElections(res, "Election succesfully deleted")
             }else{
                 await renderElections(res, "Election delete failed")
-            }
+            }*/
+           res.send(feedback);
         } catch (error) {
             console.log(error)
             await renderElections(res, "An error occurred")
         }
     })()
+})
+
+appRouter.get('/test', (req, res)=>{
+    res.setHeader('WWW-Authenticate', 'Basic realm="Access to the Site"');
+    res.status(401).send('Unauthorized')
 })
 module.exports = appRouter;
